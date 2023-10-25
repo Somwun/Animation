a98fde752e5b0ebd564826dfe0a711dd1aa610bd
Christopher Staddon <c06staddon@gmail.com>
2023-10-25 10:30:27 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animation
+{
+    internal class Tribble
+    {
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _speed;
+        public Tribble(Texture2D texture, Rectangle rect, Vector2 speed)
+        {
+            _texture = texture;
+            _rectangle = rect;
+            _speed = speed;
+        }
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+        public Rectangle Bounds
+        {
+            get { return _rectangle; }
+            set { _rectangle = value; }
+        }
+        public Vector2 Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+        public bool Move(GraphicsDeviceManager graphics)
+        {
+            bool hitTrue = false;
+            _rectangle.Offset(_speed);
+            if (_rectangle.Right > graphics.PreferredBackBufferWidth + _rectangle.Size.X || _rectangle.Left < 0 - _rectangle.Size.X)
+            {
+                hitTrue = true;
+                if (_rectangle.X > 1)
+                {
+                    _rectangle.X = 0 - _rectangle.Size.X;
+                }
+                else
+                {
+                    _rectangle.X = graphics.PreferredBackBufferWidth + _rectangle.Size.X;
+                }
+            }
+            if (_rectangle.Bottom > graphics.PreferredBackBufferHeight + _rectangle.Size.Y || _rectangle.Top < 0 - _rectangle.Size.Y)
+            {
+                hitTrue = true;
+                if (_rectangle.Y > 1)
+                {
+                    _rectangle.Y = 0 - _rectangle.Size.Y;
+                }
+                else
+                {
+                    _rectangle.Y = graphics.PreferredBackBufferWidth + _rectangle.Size.Y;
+                }
+            }
+            return hitTrue;
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Animation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _greyTribble, _orangeTribble, _brownTribble, _creamTribble, _tribbleIntro;
+        SpriteFont _introText;
+        private bool gray = false, orange = false, brown = false, cream = false;
+        private int bounces = 0;
+        Color backColor = Color.White;
+        Random generator = new Random();
+        SoundEffect _tribbleCoo;
+        MouseState mouseState;
+        Tribble tribbleGrey, tribbleOrange, tribbleBrown, tribbleCream;
+        Rectangle introRect;
+
+        enum Screen
+        {
+            Intro,
+            TribbleYard
+        }
+        Screen screen = Screen.Intro;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.ApplyChanges();
+            introRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            this.Window.Title = "Animation";
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _greyTribble = Content.Load<Texture2D>("tribbleGrey");
+            _orangeTribble = Content.Load<Texture2D>("tribbleOrange");
+            _brownTribble = Content.Load<Texture2D>("tribbleBrown");
+            _creamTribble = Content.Load<Texture2D>("tribbleCream");
+            _tribbleCoo = Content.Load<SoundEffect>("tribble_coo");
+            _tribbleIntro = Content.Load<Texture2D>("tribble_intro");
+            _introText = Content.Load<SpriteFont>("Intro");
+            tribbleGrey = new Tribble(_greyTribble, new Rectangle(generator.Next(1, 690), generator.Next(1, 390), generator.Next(50, 151), generator.Next(50, 151)), new Vector2(3, 3));
+            tribbleOrange = new Tribble(_orangeTribble, new Rectangle(generator.Next(1, 690), generator.Next(1, 390), generator.Next(50, 151), generator.Next(50, 151)), new Vector2(2, 4));
+            tribbleBrown = new Tribble(_brownTribble, new Rectangle(generator.Next(1, 690), generator.Next(1, 390), generator.Next(50, 151), generator.Next(50, 151)), new Vector2(6, 0));
+            tribbleCream = new Tribble(_creamTribble, new Rectangle(generator.Next(1, 690), generator.Next(1, 390), generator.Next(50, 151), generator.Next(50, 151)), new Vector2(0, 6));
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            mouseState = Mouse.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.TribbleYard;
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                if (tribbleGrey.Move(_graphics))
+                {
+                    backColor = Color.Gray;
+                    _tribbleCoo.Play();
+                    bounces++;
+                }
+                if (tribbleOrange.Move(_graphics))
+                {
+                    backColor = Color.Orange;
+                    _tribbleCoo.Play();
+                    bounces++;
+                }
+                if (tribbleBrown.Move(_graphics))
+                {
+                    backColor = Color.SaddleBrown;
+                    _tribbleCoo.Play();
+                    bounces++;
+                }
+                if (tribbleCream.Move(_graphics))
+                {
+                    backColor = Color.AntiqueWhite;
+                    _tribbleCoo.Play();
+                    bounces++;
+                }
+                if (bounces >= 50)
+                {
+                    screen = Screen.Intro;
+                    bounces = 0;
+                }
+            }
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(backColor);
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(_tribbleIntro, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
+                _spriteBatch.DrawString(_introText, "Click to Tribble it up", new Vector2(0, 250), Color.Black);
+            }
+            else if (screen == Screen.TribbleYard)
+            {
+                _spriteBatch.Draw(tribbleGrey.Texture, tribbleGrey.Bounds, Color.White);
+                _spriteBatch.Draw(tribbleOrange.Texture, tribbleOrange.Bounds, Color.White);
+                _spriteBatch.Draw(tribbleBrown.Texture, tribbleBrown.Bounds, Color.White);
+                _spriteBatch.Draw(tribbleCream.Texture, tribbleCream.Bounds, Color.White);
+            }
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
